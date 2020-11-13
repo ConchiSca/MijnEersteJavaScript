5c4d514d84fe53a857c8000e6220efd42223f678
ConchiSca <71380049+ConchiSca@users.noreply.github.com>
2020-11-13 16:04:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello Winc Academy");
+        let name = 'Conchi';
+        console.log(name);
+        let number = 6 / 6;
+        console.log(number);
+        let age = 'thirty';
+        console.log(typeof age);
